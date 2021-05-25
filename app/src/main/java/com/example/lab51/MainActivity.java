e692810c235546227000cdf52b00ee8e850a658b
Kirill <klarsky.kiril6@gmail.com>
2021-05-24 22:13:22 -0700

    init

---
+++ app/src/main/java/com/example/lab51/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.lab51;
+
+import android.app.ProgressDialog;
+import android.os.Build;
+import android.os.Bundle;
+
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.EditText;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @RequiresApi(api = Build.VERSION_CODES.P)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Loading Data...");
+        progressDialog.setCancelable(false);
+        WebView web_view = findViewById(R.id.web_view);
+        web_view.requestFocus();
+        web_view.getSettings().setJavaScriptEnabled(true);
+        String myPdfUrl = "www.africau.edu/images/default/sample.pdf";
+        String url = "https://docs.google.com/viewer?embedded=true&url="+myPdfUrl;
+        web_view.loadUrl(url);
+        web_view.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+        web_view.setWebChromeClient(new WebChromeClient() {
+            public void onProgressChanged(WebView view, int progress) {
+                if (progress < 100) {
+                    progressDialog.show();
+                }
+                if (progress == 100) {
+                    progressDialog.dismiss();
+                }
+            }
+        });
+    }
+}
